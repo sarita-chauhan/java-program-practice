e2bb7ccdb3621d32ea65ef08deed6c4ab670ed56
Sarita Chauhan <124147666+sarita-chauhan@users.noreply.github.com>
2023-02-01 16:19:52 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,36 @@
+
+
+public class Name{
+    public static void main(String[] args) {
+        String name="sarita";
+        Integer age=23;
+        String address="ranipur";
+        String Mail="chauhansarita000@gmail.com";
+        Float marks=45.6f;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+        System.out.println(Mail);
+        System.out.println(marks);
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Name5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Name5 {
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Your name is:");
+        String str=sc.nextLine();
+    }
+}
