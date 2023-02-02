9e179fda5b30288e67b8a7a840480a9ab759ec37
Sarita Chauhan <124147666+sarita-chauhan@users.noreply.github.com>
2023-02-02 14:11:33 +0530

    Add files via upload

---
+++ Nama6.java
@@ -0,0 +1,10 @@
+public class Nama6 {
+    public static void main(String[] args) {
+        int a=6;
+        if (a%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
