a208f5f8564d31d16affe850ec3fc43c35114ab5
Sarita Chauhan <124147666+sarita-chauhan@users.noreply.github.com>
2023-02-11 02:14:07 +0530

    Add files via upload

---
+++ Diamond.java
@@ -0,0 +1,29 @@
+public class Pattern {
+    public static void main(String[] args) {
+        
+        for(int j=0; j<5; j++){
+            for(int s=5; s>j; s--){
+                System.out.print(" ");
+            }
+            for(int i =0; i<2*j-1; i++){ 
+                System.out.print("*");
+            }
+            System.out.println();
+
+         
+    
+
+}
+for (int j=5; j>0; j--){
+    for(int s=5; s>j; s--){
+        System.out.print(" ");
+    }
+    for(int i =0; i<2*j-1; i++){ 
+        System.out.print("*");
+    }
+    System.out.println();
+}
+    }
+}
+
+
